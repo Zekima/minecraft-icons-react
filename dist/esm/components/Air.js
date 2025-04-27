1753b4508e0737fef505f2b011c5cdd42bd3e781
christian <christianledaal@gmail.com>
2025-04-27 12:34:26 +0200

    fix: complierOptions to target react

--- dist/esm/components/Air.js
+++ dist/esm/components/Air.js
@@ -9,12 +9,13 @@ var __rest = (this && this.__rest) || function (s, e) {
         }
     return t;
 };
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import * as React from 'react';
 const Air = (_a, ref) => {
     var { title } = _a, props = __rest(_a, ["title"]);
-    return (_jsxs("svg", Object.assign({ ref: ref, version: "1.1", xmlns: "http://www.w3.org/2000/svg", width: "32", height: "32", viewBox: "0 0 32 32", "shape-rendering": "crispEdges" // Inject the original attributes (width, height, viewBox, etc.)
-     }, props, { children: [title ? _jsx("title", { children: title }) : null, _jsx("g", { dangerouslySetInnerHTML: { __html: `` } })] })));
+    return (React.createElement("svg", Object.assign({ ref: ref, version: "1.1", xmlns: "http://www.w3.org/2000/svg", width: "32", height: "32", viewBox: "0 0 32 32", "shape-rendering": "crispEdges" // Inject the original attributes (width, height, viewBox, etc.)
+     }, props),
+        title ? React.createElement("title", null, title) : null,
+        React.createElement("g", { dangerouslySetInnerHTML: { __html: `` } })));
 };
 // Use React.forwardRef to pass the ref down to the SVG element
 const ForwardRefComponent = React.forwardRef(Air);
